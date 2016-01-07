5413c1b098260a3bfef7a81ff69460702603b869
Bret Doucette <doucette.bret@gmail.com>
2016-01-07 09:48:27 -0700

    ADDED MORE SPEEEEDDgit add .

--- lib/denver.rb
+++ lib/denver.rb
@@ -3,8 +3,7 @@ require_relative 'parser'
 class Denver
 
   def initialize
-    @data = Parser.new.parse_csv("./data/traffic-accidents.csv", :incident_address)
-    @data = Parser.new.parse_csv("./data/traffic-accidents.csv", :neighborhood_id)
+    @data = Parser.new.parse_csv("./data/traffic-accidents.csv", :incident_address, :neighborhood_id)
     @data = Parser.new.parse_csv("./data/crime.csv", :neighborhood_id)
   end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -3,12 +3,14 @@ require 'pry'
 require_relative 'dangerous_sorter'
 
 class Parser
-  def parse_csv(filename, attribute)
-    information = []
-    CSV.foreach(filename,:headers => true, :header_converters => :symbol,
-    :converters => :numeric) do |row|
-      information << row[attribute]
+  def parse_csv(filename, attribute_one = nil, attribute_two = nil)
+    information_one = []
+    information_two = []
+    CSV.foreach(filename,:headers => true, :header_converters => :symbol) do |row|
+      information_one << row[attribute_one]
+      information_two << row[attribute_two]
     end
-    DangerousSorter.new(information.compact)
+    DangerousSorter.new(information_one.compact)
+    DangerousSorter.new(information_two.compact)
   end
 end
