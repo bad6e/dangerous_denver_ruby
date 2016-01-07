a57e43fc7440e43915fb28cb236af8791e991ada
Bret Doucette <doucette.bret@gmail.com>
2016-01-07 09:25:58 -0700

    Refactored

--- lib/denver.rb
+++ lib/denver.rb
@@ -3,8 +3,9 @@ require_relative 'parser'
 class Denver
 
   def initialize
-    @data = Parser.new("./data/traffic-accidents.csv").parse_csv
-    @crime_data = Parser.new("./data/crime.csv").parse_crime_csv
+    @data = Parser.new.parse_csv("./data/traffic-accidents.csv", :incident_address)
+    @data = Parser.new.parse_csv("./data/traffic-accidents.csv", :neighborhood_id)
+    @data = Parser.new.parse_csv("./data/crime.csv", :neighborhood_id)
   end
 end
 
--- lib/parser.rb
+++ lib/parser.rb
@@ -3,29 +3,12 @@ require 'pry'
 require_relative 'dangerous_sorter'
 
 class Parser
-
-  def initialize(filename)
-    @filename = filename
-  end
-
-  def parse_csv
-    neighborhood = []
-    address = []
-    CSV.foreach(@filename,:headers => true, :header_converters => :symbol,
+  def parse_csv(filename, attribute)
+    information = []
+    CSV.foreach(filename,:headers => true, :header_converters => :symbol,
     :converters => :numeric) do |row|
-      neighborhood << row[:neighborhood_id]
-      address << row[:incident_address]
+      information << row[attribute]
     end
-    DangerousSorter.new(address.compact)
-    DangerousSorter.new(neighborhood.compact)
-  end
-
-  def parse_crime_csv
-    crime = []
-    CSV.foreach(@filename,:headers => true, :header_converters => :symbol,
-    :converters => :numeric) do |row|
-      crime << row[:neighborhood_id]
-    end
-    DangerousSorter.new(crime.compact)
+    DangerousSorter.new(information.compact)
   end
 end
